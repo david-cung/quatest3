40cc628282cdbf6806132d0c9d4a73e2deb0f7d6
davidCung <vancung1951997@gmail.com>
2025-02-25 06:44:37 +0700

    Add responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,13 +41,13 @@ function HomePage() {
   };
 
   return (
-    <div style={{ backgroundColor: "#fafafa",  paddingTop: "120px" }}>
+    <div style={{ backgroundColor: "#fafafa", paddingTop: "120px", padding: "10px" }}>
       <AutoSweetImages />
       
       <div style={{
-        display: "flex", maxWidth: "1200px", margin: "40px auto", gap: "20px", alignItems: "center"
+        display: "flex", flexWrap: "wrap", maxWidth: "1200px", margin: "40px auto", gap: "20px", alignItems: "center"
       }}>
-        <div style={{ flex: "2" }}>
+        <div style={{ flex: "2", minWidth: "300px" }}>
           <h2 style={{ fontSize: "22px", color: "#555", fontWeight: "bold" }}>GIỚI THIỆU VỀ INTEST</h2>
           <h1 style={{ fontSize: "28px", color: "#222", fontWeight: "bold", margin: "10px 0" }}>
             INTEST là đơn vị cung cấp dịch vụ hiệu chuẩn thiết bị, máy móc đạt
@@ -70,7 +70,7 @@ function HomePage() {
           </ul>
         </div>
 
-        <div style={{ maxWidth: "400px", textAlign: "center" }}>
+        <div style={{ maxWidth: "400px", textAlign: "center", minWidth: "300px" }}>
           <img
             src={service10}
             alt="Thiết bị đo lường"
@@ -86,27 +86,20 @@ function HomePage() {
         </div>
       </div>
 
-      <div style={{ marginLeft: "300px", marginRight: "300px",  textAlign: "justify" }}>
-      <p
-        style={{
-          fontSize: "18px",
-          color: "#555",
-          lineHeight: "1.8",
-          margin: "20px 0",
-        }}
-      >
-        INTEST với đội ngũ nhân viên có kinh nghiệm lâu năm trong lĩnh vực hiệu chuẩn đo lường, 
-        được đào tạo chuyên môn ở Viện Đo Lường Việt Nam (VMI), Trung tâm Kỹ thuật Tiêu Chuẩn Đo lường 
-        Chất lượng 3 (QUATEST 3), Trung Tâm Đào Tạo Nghiệp Vụ Tiêu Chuẩn Đo Lường Chất Lượng (QTC) 
-        cùng với sự đầu tư về máy móc, thiết bị hiện đại.
-        <br /><br />
-        Chúng tôi luôn luôn phát triển để nâng cao kiến thức và chất lượng dịch vụ, lắng nghe và tư vấn 
-        cho khách hàng giải pháp tối ưu nhất mang lại hiệu quả kinh tế cao. Luôn đảm bảo tuân thủ theo 
-        các yêu cầu về quản lý và năng lực kỹ thuật cùng với hệ thống quản lý chất lượng để đưa ra kết 
-        quả đo lường có độ tin cậy cao được Quốc Tế thừa nhận.
-        <br /><br />
-        Cảm ơn Quý Khách hàng đã luôn tin tưởng và ủng hộ chúng tôi!
-      </p>
+      <div style={{ margin: "20px", textAlign: "justify" }}>
+        <p style={{ fontSize: "18px", color: "#555", lineHeight: "1.8", margin: "20px 0" }}>
+          INTEST với đội ngũ nhân viên có kinh nghiệm lâu năm trong lĩnh vực hiệu chuẩn đo lường, 
+          được đào tạo chuyên môn ở Viện Đo Lường Việt Nam (VMI), Trung tâm Kỹ thuật Tiêu Chuẩn Đo lường 
+          Chất lượng 3 (QUATEST 3), Trung Tâm Đào Tạo Nghiệp Vụ Tiêu Chuẩn Đo Lường Chất Lượng (QTC) 
+          cùng với sự đầu tư về máy móc, thiết bị hiện đại.
+          <br /><br />
+          Chúng tôi luôn luôn phát triển để nâng cao kiến thức và chất lượng dịch vụ, lắng nghe và tư vấn 
+          cho khách hàng giải pháp tối ưu nhất mang lại hiệu quả kinh tế cao. Luôn đảm bảo tuân thủ theo 
+          các yêu cầu về quản lý và năng lực kỹ thuật cùng với hệ thống quản lý chất lượng để đưa ra kết 
+          quả đo lường có độ tin cậy cao được Quốc Tế thừa nhận.
+          <br /><br />
+          Cảm ơn Quý Khách hàng đã luôn tin tưởng và ủng hộ chúng tôi!
+        </p>
       </div>
 
       <Services />
